ab671d7332cfc7d6288ed47e3bb78eb31c6b2761
rdw100 <ryandustinwright@hotmail.com>
2021-01-11 17:27:26 -0500

    Cleaned up UoW file.

--- ORM.Dapper.Infrastructure/Repositories/UnitOfWork.cs
+++ ORM.Dapper.Infrastructure/Repositories/UnitOfWork.cs
@@ -10,14 +10,7 @@ namespace ORM.Dapper.Infrastructure.Repositories
             Regions = regionRepository;
         }
 
-        //private IRegionRepository _regions;
-
         public IShipperRepository Shippers { get; }
-       // public IRegionRepository Regions => _regions;
         public IRegionRepository Regions { get; }
-        //public IRegionRepository Regions
-        //{
-        //    get { return _regions ?? (_regions = new RegionRepository()); }
-        //}
     }
 }
